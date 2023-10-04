7dea07b500b25a0be8ca939e7ca1fd9053b4b116
Selim Hasenekoğlu <hasenek2000@gmail.com>
2023-10-04 21:25:31 +0300

    newPush

---
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+}
