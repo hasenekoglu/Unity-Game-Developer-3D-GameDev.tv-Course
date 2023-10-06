e636e8eb899cfeca64317bf4d4b520ff53390f6f
Selim Hasenekoğlu <hasenek2000@gmail.com>
2023-10-06 23:51:26 +0300

    commit

--- Obstacle Course/Assets/Scripts/Dropper.cs
+++ Obstacle Course/Assets/Scripts/Dropper.cs
@@ -2,33 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Dropper : MonoBehaviour
 {
 
     public GameObject[] gameObjects;
-    int objectIndex = 0;
+    int currentIndex = 0;
+    float triggerTime;
+    bool triggered = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !triggered)
+
         {
-            if (objectIndex < gameObjects.Length)
-            {
-                if (objectIndex > 0)
-                {
-                    Rigidbody rigidbody = gameObjects[objectIndex - 1].GetComponent<Rigidbody>();
-                    rigidbody.useGravity = true;
+            triggered = true;
+            triggerTime = Time.time;
 
-                }
-                Rigidbody currentRigidBody = gameObjects[objectIndex].GetComponent<Rigidbody>();
-                currentRigidBody.useGravity = false;
-                objectIndex++;
-            }
+            BoxDropped(currentIndex);
 
-
-            Debug.Log("çarptın");
         }
 
     }
+    void Update()
+    {
+        BoxDelay();
+    }
+
+    void BoxDelay()
+    {
+        float delayTime = Time.time - triggerTime;
+        if (triggered)
+        {
+            if (delayTime >= .5f)
+            {
+                if (currentIndex < gameObjects.Length)
+                {
+                    BoxDropped(currentIndex);
+                    currentIndex++;
+                    triggerTime = Time.time;
+                }
+            }
+        }
+
+
+    }
+    void BoxDropped(int index)
+    {
+        if (index >= 0 && index < gameObjects.Length)
+        {
+            Rigidbody rb = gameObjects[index].GetComponent<Rigidbody>();
+            rb.useGravity = true;
+        }
+    }
 }
+
+
--- Obstacle Course/Assets/Scripts/ObjectHit.cs
+++ Obstacle Course/Assets/Scripts/ObjectHit.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other)
     {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.blue;
+            gameObject.tag = "Hit";
+        }
 
-        GetComponent<MeshRenderer>().material.color = Color.blue;
     }
 }
